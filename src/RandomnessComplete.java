e7321b01016979b903cb754b29710b9a6bffca43
Jay Wu <wu274319426@gmail.com>
2017-06-22 15:55:39 -0400

    RandomDice

---
+++ src/RandomnessComplete.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class RandomnessComplete {
+
+	public static void main(String[] args) {
+		
+
+		        int dienumber;
+		        Random rnd = new Random();
+		        
+		        dienumber = rnd.nextInt(6) + 1;
+		        
+		        System.out.println("Your die roll was : " + dienumber);
+		    }
+
+	}
+
